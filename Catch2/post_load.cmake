560715f7176321971fe40e03d4b64c1929f85fd6
Skylar Cupit <Skylarcupit@gmail.com>
2022-07-24 18:48:27 -0500

    Transition to new system where each target is given a config

---
+++ Catch2/post_load.cmake
@@ -0,0 +1,1 @@
+list( APPEND CMAKE_MODULE_PATH "${catch2_SOURCE_DIR}/extras" )
---
+++ Catch2/pre_load.cmake
@@ -0,0 +1,2 @@
+set( CATCH_BUILD_TESTING OFF CACHE BOOL "Whether to build Catch2's self-test project. GCMake sets this to OFF by default." )
+set( CATCH_INSTALL_DOCS OFF CACHE BOOL "Whether to enable installation of Catch2's documentation. GCMake sets this to OFF by default." )
