1f27b4463345e31810c341d6526427af5360686e
Skylar Cupit <Skylarcupit@gmail.com>
2022-09-10 15:26:36 -0500

    Added ftxui support

---
+++ FTXUI/pre_load.cmake
@@ -0,0 +1,2 @@
+option( FTXUI_BUILD_DOCS "Set to ON to build FTXUI docs. GCMake sets this to OFF by default." OFF )
+option( FTXUI_BUILD_EXAMPLES "Set to ON to build FTXUI examples. GCMake sets this to OFF by default." OFF )
