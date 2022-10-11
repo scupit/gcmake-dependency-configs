eef95c4e4b5e0477ac087aa7de45b3df782f1e97
Skylar Cupit <Skylarcupit@gmail.com>
2022-10-11 18:31:47 -0500

    Added support for spdlog

---
+++ spdlog/pre_load.cmake
@@ -0,0 +1,2 @@
+# fmt::fmt is added as an external requirement of spdlog already, so GCMake guarantees this will be fine.
+option( SPDLOG_FMT_EXTERNAL "Use external fmt library instead of bundled" ON )
