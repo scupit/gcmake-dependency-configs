419fbd53e119a064f22d0109001995c9a52f9559
Skylar Cupit <Skylarcupit@gmail.com>
2023-10-30 13:22:46 -0500

    Added support for raylib

---
+++ raylib/pre_load.cmake
@@ -0,0 +1,6 @@
+set( USE_EXTERNAL_GLFW "OFF" CACHE STRING "Set to OFF by GCMake. We actually are using external GLFW, but this avoids an unnecesary find_package call in raylib. glfw3_FOUND is set by GCMake so that raylib still links against GLFW targets. See raylib's Glfwimport.cmake file for details.")
+set( glfw3_FOUND TRUE CACHE BOOL "Set to TRUE by GCMake. Ensures raylib links against glfw targets." )
+
+# Don't FORCE this one, since BUILD_EXAMPLES is a pretty general variable name
+# and could be set by other external project CMake code.
+set( BUILD_EXAMPLES FALSE CACHE BOOL "Set to OFF so raylib examples aren't built." )
