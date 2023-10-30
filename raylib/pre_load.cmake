62183d4e80c007e5a5a84d2d7c0e115ae53f0ad7
Skylar Cupit <Skylarcupit@gmail.com>
2023-10-30 14:59:46 -0500

    raylib configuration now supports emscripten

--- raylib/pre_load.cmake
+++ raylib/pre_load.cmake
@@ -1,3 +1,7 @@
+if( USING_EMSCRIPTEN )
+  set( PLATFORM "Web" CACHE STRING "raylib platform is set to 'Web' when building with Emscripten." FORCE )
+endif()
+
 set( USE_EXTERNAL_GLFW "OFF" CACHE STRING "Set to OFF by GCMake. We actually are using external GLFW, but this avoids an unnecesary find_package call in raylib. glfw3_FOUND is set by GCMake so that raylib still links against GLFW targets. See raylib's Glfwimport.cmake file for details.")
 set( glfw3_FOUND TRUE CACHE BOOL "Set to TRUE by GCMake. Ensures raylib links against glfw targets." )
 
